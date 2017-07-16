d665ce38235490895f5e5a7c20b22d56750574f9
David Banks <dave@hoglet.com>
2017-07-16 10:38:10 +0100

    Make zoom in/zoom out controls edge triggered
    Change-Id: I2796642488fe7b08cd61ff875571ef67800c489c

--- src/scheduler.vhd
+++ src/scheduler.vhd
@@ -7,11 +7,11 @@ entity scheduler is
            base_addr_word : out std_logic_vector(17 downto 0);
            x              : out STD_LOGIC_VECTOR (9 downto 0);
            y              : out STD_LOGIC_VECTOR (9 downto 0);
-           -- 
+           --
            newvalue       : out STD_LOGIC_VECTOR (35 downto 0);
            storex         : out STD_LOGIC;
            storey         : out STD_LOGIC;
-           
+
            accepted       : in  STD_LOGIC;
            vsync          : in  STD_LOGIC;
            btn_up         : in  STD_LOGIC;
@@ -22,7 +22,7 @@ entity scheduler is
 end scheduler;
 
 architecture Behavioral of scheduler is
-   type fsm_state is ( idle, update_constants_x_start, update_constants_x, 
+   type fsm_state is ( idle, update_constants_x_start, update_constants_x,
                              update_constants_y_start, update_constants_y,
                              start_redraw, redraw);
    signal mode                : fsm_state := update_constants_x_start;
@@ -31,7 +31,7 @@ architecture Behavioral of scheduler is
    signal vsync_reclock       : std_logic_vector(1 downto 0) := (others => '0');
    signal base_addr_word_sig  : std_logic_vector(17 downto 0) := (others => '0');
    signal base_addr_word_next : std_logic_vector(17 downto 0) := (others => '0');
-   
+
    -- where we are on the screen
    signal redraw_right          : std_logic_vector(9 downto 0) := "1001110011";
    signal redraw_bottom         : std_logic_vector(9 downto 0) := "1100111011";
@@ -49,8 +49,9 @@ architecture Behavioral of scheduler is
 
    signal redraw_top         : std_logic_vector(9 downto 0) := (others => '0');
    signal redraw_left        : std_logic_vector(9 downto 0) := (others => '0');
-   
+
    signal buttons : STD_LOGIC_VECTOR (4 downto 0);
+   signal buttons_old : STD_LOGIC_VECTOR (4 downto 0);
    -- Used for setting the constants
    signal storex_sig         : std_logic := '0';
    signal storey_sig         : std_logic := '0';
@@ -68,7 +69,7 @@ begin
    begin
       if rising_edge(clk) then
          base_addr_word_sig <= base_addr_word_next;
-         case mode is 
+         case mode is
             when update_constants_x_start =>
                mode         <= update_constants_x;
                x_counter    <= (others => '0');
@@ -77,31 +78,31 @@ begin
 
             when update_constants_x =>
                if accepted = '1' then
-                  if x_counter = 827 then 
+                  if x_counter = 827 then
                      mode         <= update_constants_y_start;
                   end if;
                   storex_sig   <= '1';
                   x_counter    <= x_counter+1;
                   newvalue_sig <= newvalue_sig + current_scale;
                end if;
-               
+
             when update_constants_y_start =>
                mode         <= update_constants_y;
                storex_sig   <= '0';
                storey_sig   <= '1';
                newvalue_sig <= current_top;
                y_counter    <= (others => '0');
-               
+
             when update_constants_y =>
                if accepted = '1' then
                   newvalue_sig <= newvalue_sig + current_scale;
                   storey_sig <= '1';
-                  if y_counter = 627 then 
+                  if y_counter = 627 then
                      mode <= start_redraw;
                   end if;
                   y_counter  <= y_counter+1;
                end if;
-               
+
             when start_redraw =>
                mode         <= redraw;
                storey_sig   <= '0';
@@ -110,7 +111,7 @@ begin
 
             when redraw =>
                if accepted = '1' then
-                  if x_counter = redraw_right then 
+                  if x_counter = redraw_right then
                      x_counter <= redraw_left;
                      if y_counter = redraw_bottom then
                         mode      <= idle;
@@ -122,15 +123,18 @@ begin
                   end if;
                end if;
 
-            when others =>      -- idle 
+            when others =>      -- idle
                if vsync_reclock = "01" then
+
+                  buttons_old   <= buttons;
+
                   redraw_top    <= (others => '0');
                   redraw_left   <= (others => '0');
                   redraw_right  <= "0000000000"+827;
                   redraw_bottom <= "0000000000"+627;
-                  
-                  
-                  case buttons is 
+
+
+                  case buttons is
                   when "10000" =>
                        base_addr_word_next <= base_addr_word_sig + (14 * 828)/2;
                        current_top         <= current_top + current_scale_add_reg;
@@ -149,16 +153,16 @@ begin
                         redraw_left <= "0000000000"+814;
 
                   when "10001" =>
-                        if  current_scale(27) = '0' then                        
+                        if  current_scale(27) = '0' and buttons_old(4) = '0' then
                            current_top   <= current_top_zoom_out;
                            current_left  <= current_left_zoom_out;
                            current_scale <= current_scale(26 downto 0) & '0';
                            zoom_x_offset <= zoom_x_offset(34 downto 0) & '0';
                            zoom_y_offset <= zoom_y_offset(34 downto 0) & '0';
                         end if;
-                        
+
                   when "01001" =>
-                        if current_scale(0) = '0' then
+                        if current_scale(0) = '0' and buttons_old(3) = '0' then
                            current_top   <= current_top_zoom_in;
                            current_left  <= current_left_zoom_in;
                            current_scale <= '0' & current_scale(27 downto 1);
@@ -167,16 +171,16 @@ begin
                         end if;
                   when others =>
                   end case;
-                  
+
                   if (btn_down or btn_up or btn_left or btn_right) = '1' then
                      mode                <= update_constants_x_start;
                   end if;
                end if;
          end case;
-         
+
          current_scale_add_reg <= current_scale_add;
          current_scale_add <= ("0" & current_scale & "0000") - (current_scale & "0");
-         
+
          current_top_zoom_out  <= current_top  - zoom_y_offset(34 downto 0);
          current_left_zoom_out <= current_left - zoom_x_offset(34 downto 0);
          current_top_zoom_in   <= current_top  + zoom_y_offset(35 downto 1);
