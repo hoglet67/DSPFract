597662f2fc765907fc6be88d5c3452093fc5f43a
David Banks <dave@hoglet.com>
2019-04-03 12:35:17 +0100

    Dave/Ed experiments: replaced third multiplier with simple LUT, as only 4 bits needed
    Change-Id: I22536ccc42343b26e03fb06a8fa96152e4a29e63

--- src/sqr35.vhd
+++ src/sqr35.vhd
@@ -10,33 +10,24 @@ end sqr35;
 
 architecture Behavioral of sqr35 is
 
---  COMPONENT ip_mult35
---    PORT (
---      clk : IN  STD_LOGIC;
---      a   : IN  STD_LOGIC_VECTOR(34 DOWNTO 0);
---      b   : IN  STD_LOGIC_VECTOR(34 DOWNTO 0);
---      p   : OUT STD_LOGIC_VECTOR(37 DOWNTO 0)
---    );
---  END COMPONENT;
-
-signal pa0 : std_logic_vector(35 downto 0);
-signal pb0 : std_logic_vector(35 downto 0);
-signal pb1 : std_logic_vector(35 downto 0);
-signal pc0 : std_logic_vector(35 downto 0);
-signal pc1 : std_logic_vector(35 downto 0);
-signal pc2 : std_logic_vector(35 downto 0);
+signal pa2 : std_logic_vector(35 downto 0);
+signal pb2 : std_logic_vector(35 downto 0);
+signal pb3 : std_logic_vector(35 downto 0);
+signal pc1 : std_logic_vector(3 downto 0);
+signal pc2 : std_logic_vector(3 downto 0);
+signal pc3 : std_logic_vector(3 downto 0);
+signal pc4 : std_logic_vector(3 downto 0);
 
 signal xh : std_logic_vector(17 downto 0);
 signal xl : std_logic_vector(17 downto 0);
 
-signal sum1 : std_logic_vector(37 downto 0);
-signal sum2 : std_logic_vector(37 downto 0);
 signal sum3 : std_logic_vector(37 downto 0);
 signal sum4 : std_logic_vector(37 downto 0);
 signal sum5 : std_logic_vector(37 downto 0);
+signal sum6 : std_logic_vector(37 downto 0);
+signal sum7 : std_logic_vector(37 downto 0);
 
-signal padb1 : std_logic_vector(15 downto 0); -- 16 bits
-signal padc2 : std_logic_vector(33 downto 0); -- 34 bits
+signal padb3 : std_logic_vector(15 downto 0); -- 16 bits
 
 begin
 
@@ -59,7 +50,7 @@ multipliera : entity work.ip_mult18
     clk => clk,
     a => xh,
     b => xh,
-    p => pa0
+    p => pa2
   );
 
 multiplierb : entity work.ip_mult18
@@ -67,32 +58,67 @@ multiplierb : entity work.ip_mult18
     clk => clk,
     a => xh,
     b => xl,
-    p => pb0
+    p => pb2
   );
 
-multiplierc : entity work.ip_mult18
-  PORT MAP (
-    clk => clk,
-    a => xl,
-    b => xl,
-    p => pc0
-  );
 
-padb1 <= (others => pb1(35));
-padc2 <= (others => pc2(35));
+padb3 <= (others => pb3(35));
 
 process(clk)
 begin
     if rising_edge(clk) then
-        pb1 <= pb0;
-        pc1 <= pc0;
+        case xl(16 downto 12) is
+            when "01000" => pc1 <= x"1";
+            when "01001" => pc1 <= x"1";
+            when "01010" => pc1 <= x"1";
+            when "01011" => pc1 <= x"1";
+            when "01100" => pc1 <= x"2";
+            when "01101" => pc1 <= x"2";
+            when "01110" => pc1 <= x"3";
+            when "01111" => pc1 <= x"3";
+            when "10000" => pc1 <= x"4";
+            when "10001" => pc1 <= x"4";
+            when "10010" => pc1 <= x"5";
+            when "10011" => pc1 <= x"5";
+            when "10100" => pc1 <= x"6";
+            when "10101" => pc1 <= x"6";
+            when "10110" => pc1 <= x"7";
+            when "10111" => pc1 <= x"8";
+            when "11000" => pc1 <= x"9";
+            when "11001" => pc1 <= x"9";
+            when "11010" => pc1 <= x"A";
+            when "11011" => pc1 <= x"B";
+            when "11100" => pc1 <= x"C";
+            when "11101" => pc1 <= x"D";
+            when "11110" => pc1 <= x"E";
+            when "11111" => pc1 <= x"F";
+            when others  => pc1 <= x"0";
+        end case;
+
+        -- stage 2
         pc2 <= pc1;
-        sum1 <= pa0 & "00";
-        sum2 <= sum1 + (padb1 & pb1(35 downto 14));
-        sum3 <= sum2 + pc2(33 downto 30);  -- 35..29 looks perfect (with 2 bits of eounding)
-        sum4 <= sum3;
-        sum5 <= sum4;
-        result <= sum5;
+
+        -- statge 3
+        pb3 <= pb2;
+        pc3 <= pc2;
+        sum3 <= pa2 & "00";
+
+        -- stage 4
+        pc4 <= pc3;
+        sum4 <= sum3 + (padb3 & pb3(35 downto 14));
+
+        -- stage 5
+        sum5 <= sum4 + pc4;
+
+        -- stage 6
+        sum6 <= sum5;
+
+        -- stage 7
+        sum7 <= sum6;
+
+        -- stage 8
+        result <= sum7;
+
     end if;
 end process;
 
